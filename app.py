a6740a3969664ef98e67082d339a048d0b84e28e
Bhumi2004 <115335044+Bhumi2004@users.noreply.github.com>
2025-02-28 11:41:23 +0530

    Update app.py

--- app.py
+++ app.py
@@ -159,7 +159,7 @@ def video_feed():
 
 if __name__ == '__main__':
     try:
-        port = int(os.environ.get("PORT", 5000))  # Get Render's assigned port
+        port = int(os.environ.get("PORT", 1000))  # Get Render's assigned port
         app.run(host="0.0.0.0", port=port, debug=True)
     finally:
         cap.release()
